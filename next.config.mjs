ddac59900a36ddb41e5f5532967bbbf3d2e01702
Mariano <marianoberton@gmail.com>
2024-06-07 15:35:21 -0300

    Limpieza del repositorio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com'],
+    },
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+          path: require.resolve('path-browserify'),
+        };
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
