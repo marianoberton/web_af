3b60d793dfe328ccc4ac275557c826ab9166297d
Mariano <marianoberton@gmail.com>
2024-08-05 15:18:48 -0300

    cambio de hero sin archivos grandes

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <div className="relative h-[70vh] bg-cover bg-center" style={{ backgroundImage: 'url(/images/hero-background11.jpg)' }}>
+    <div className="relative h-[70vh] bg-cover bg-center" style={{ backgroundImage: 'url(/images/pastor.jpg)' }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative z-10 container mx-auto flex items-end md:items-center h-full text-white p-5">
         <div className="w-full md:max-w-lg text-right ml-auto mb-4 md:mb-0">
